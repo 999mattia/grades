b07bcfe45dc05ec8b5ba8a8f9854b679018cc472
999mattia <mattia.gisiger@gmail.com>
2023-05-11 19:00:58 +0200

    added models

--- app.go
+++ app.go
@@ -6,10 +6,33 @@ import (
 	"gorm.io/gorm"
 )
 
+type Grade struct {
+	gorm.Model
+	Name     string
+	Grade    float32
+	ModuleId int
+	UserId   int
+}
+
+type Module struct {
+	gorm.Model
+	Name     string
+	Semester int
+	UserId   int
+}
+
+type User struct {
+	gorm.Model
+	Name     string
+	Password string
+}
+
 var DB *gorm.DB
 
 func main() {
 	DB, _ = gorm.Open(postgres.Open("postgres://jwnybpvo:tvKkBSR1LoQiQSBtsw5IA2o9bUNfTXuY@kandula.db.elephantsql.com/jwnybpvo"), &gorm.Config{})
+
+	DB.AutoMigrate(&User{}, &Module{}, &Grade{})
 
 	r := gin.Default()
 
