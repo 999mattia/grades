8009ce983626ed2a60a53fb2cfe6991b96710bcf
999mattia <mattia.gisiger@gmail.com>
2023-05-15 19:58:06 +0200

    refactored cors

--- app.go
+++ app.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/999mattia/grades/controllers"
 	"github.com/999mattia/grades/db"
 	"github.com/999mattia/grades/middleware"
@@ -16,19 +14,7 @@ func main() {
 
 	r := gin.Default()
 
-	r.Use(func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Authorization")
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-
-		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(http.StatusOK)
-			return
-		}
-
-		c.Next()
-	})
+	r.Use(middleware.CORS)
 
 	r.POST("/api/signup", controllers.SignUp)
 	r.POST("/api/login", controllers.Login)
