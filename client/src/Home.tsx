7b16a8b337a4491fee907eeb05cf803d92eee59f
999mattia <mattia.gisiger@gmail.com>
2023-05-18 15:51:03 +0200

    initial commit

--- client/src/Home.tsx
+++ client/src/Home.tsx
@@ -1,7 +1,13 @@
 import type { Component } from "solid-js";
+import { Routes, Route, A } from "@solidjs/router";
 
 const Home: Component = () => {
-	return <>Home</>;
+	return (
+		<>
+			Home
+			<A href="/login">Login</A>
+		</>
+	);
 };
 
 export default Home;
